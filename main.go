8fe8d9b35d60ae955e3adc2aaf24ead18242e35a
Joshua Lucas <joshuamlucas91@gmail.com>
2025-08-26 10:56:51 -0400

    refactor: Add logic to capture user input

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanner.Scan()
+
+		rawUserInput := scanner.Text()
+
+		cleanedUserInput := cleanInput(rawUserInput)
+
+		fmt.Printf("Your command was: %s\n", cleanedUserInput[0])
+	}
+
 }
 
 func cleanInput(text string) []string {
