6aa9804f66cc363640400714d3167b497cdc0db4
Joshua Lucas <joshuamlucas91@gmail.com>
2025-08-27 08:19:42 -0400

    feat: add commands to cli pokedex program

--- main.go
+++ main.go
@@ -8,6 +8,19 @@ import (
 )
 
 func main() {
+	CLI_COMMANDS := map[string]cliCommand{}
+
+	CLI_COMMANDS["exit"] = cliCommand{
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	}
+
+	CLI_COMMANDS["help"] = cliCommand{
+		name:        "help",
+		description: "Provides usage details to the user",
+		callback:    commandHelp(CLI_COMMANDS),
+	}
 
 	scanner := bufio.NewScanner(os.Stdin)
 
@@ -20,7 +33,17 @@ func main() {
 
 		cleanedUserInput := cleanInput(rawUserInput)
 
-		fmt.Printf("Your command was: %s\n", cleanedUserInput[0])
+		for _, val := range cleanedUserInput {
+
+			command, ok := CLI_COMMANDS[val]
+
+			if ok {
+				command.callback()
+			} else {
+				fmt.Println("Unknown command")
+			}
+
+		}
 	}
 
 }
@@ -33,4 +56,37 @@ func cleanInput(text string) []string {
 	}
 
 	return cleanedInput
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandHelp(commands map[string]cliCommand) func() error {
+
+	return func() error {
+		fmt.Println("Welcome to the Pokedex!")
+		fmt.Println("Usage: ")
+		fmt.Println("")
+
+		for _, val := range commands {
+			if val.name == "help" {
+				continue
+			}
+
+			fmt.Printf("%s: %s\n", val.name, val.description)
+
+		}
+		return nil
+	}
+
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+
+	return nil
 }
