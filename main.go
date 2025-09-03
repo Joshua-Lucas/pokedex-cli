ead0f13672ecfc2ea09f2e8aec02127ab507161a
Joshua Lucas <joshuamlucas91@gmail.com>
2025-09-03 08:19:31 -0400

    feat: Add logic get map locations

--- main.go
+++ main.go
@@ -3,23 +3,34 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/Joshua-Lucas/pokedex-cli/internal/pokeapi"
 	"os"
 	"strings"
 )
 
 func main() {
 	CLI_COMMANDS := map[string]cliCommand{}
+	CONFIG := config{
+		Next:     "https://pokeapi.co/api/v2/location-area/",
+		Previous: "",
+	}
 
 	CLI_COMMANDS["exit"] = cliCommand{
 		name:        "exit",
 		description: "Exit the Pokedex",
-		callback:    commandExit,
+		callback:    commandExit(&CONFIG),
 	}
 
 	CLI_COMMANDS["help"] = cliCommand{
 		name:        "help",
 		description: "Provides usage details to the user",
-		callback:    commandHelp(CLI_COMMANDS),
+		callback:    commandHelp(&CONFIG, CLI_COMMANDS),
+	}
+
+	CLI_COMMANDS["map"] = cliCommand{
+		name:        "map",
+		description: "Displays map locations in our pokeman world",
+		callback:    commandMap(&CONFIG),
 	}
 
 	scanner := bufio.NewScanner(os.Stdin)
@@ -64,7 +75,30 @@ type cliCommand struct {
 	callback    func() error
 }
 
-func commandHelp(commands map[string]cliCommand) func() error {
+type config struct {
+	Next     string
+	Previous string
+}
+
+func (c *config) getNext() error {
+	locations, err := pokeapi.GetLocations(c.Next)
+	if err != nil {
+		return fmt.Errorf("Error occurred when fetching map locations: %v", err)
+	}
+
+	// Set next config
+	c.Next = locations.Next
+	c.Previous = locations.Previous
+
+	// Print content
+	for _, loc := range locations.Results {
+		fmt.Println(loc.Name)
+	}
+
+	return nil
+}
+
+func commandHelp(cfg *config, commands map[string]cliCommand) func() error {
 
 	return func() error {
 		fmt.Println("Welcome to the Pokedex!")
@@ -84,9 +118,25 @@ func commandHelp(commands map[string]cliCommand) func() error {
 
 }
 
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
+func commandExit(cfg *config) func() error {
 
-	return nil
+	return func() error {
+		fmt.Println("Closing the Pokedex... Goodbye!")
+		os.Exit(0)
+
+		return nil
+	}
+}
+
+func commandMap(cfg *config) func() error {
+
+	return func() error {
+
+		err := cfg.getNext()
+		if err != nil {
+			return fmt.Errorf("Error getting next locations: %v", err)
+		}
+
+		return nil
+	}
 }
