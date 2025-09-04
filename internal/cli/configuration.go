472709a9e8496728869421b96f384f8702537758
Joshua Lucas <joshuamlucas91@gmail.com>
2025-09-04 07:49:39 -0400

    refactor: Config logic out of main and into cli package

---
+++ internal/cli/configuration.go
@@ -0,0 +1,57 @@
+package cli
+
+import (
+	"fmt"
+	"github.com/Joshua-Lucas/pokedex-cli/internal/pokeapi"
+)
+
+type Config struct {
+	Next     string
+	Previous string
+}
+
+func (c *Config) getNext() error {
+	locations, err := pokeapi.GetLocations(c.Next)
+	if err != nil {
+		return fmt.Errorf("Error occurred when fetching map locations: %v", err)
+	}
+
+	// Set next config
+	c.Next = locations.Next
+	c.Previous = locations.Previous
+
+	// Print content
+	for _, loc := range locations.Results {
+		fmt.Println(loc.Name)
+	}
+
+	return nil
+}
+
+func (c *Config) getPrev() error {
+	onFirstPage := false
+	if c.Previous == "" {
+		c.Previous = "https://pokeapi.co/api/v2/location-area/"
+		onFirstPage = true
+	}
+
+	locations, err := pokeapi.GetLocations(c.Previous)
+	if err != nil {
+		return fmt.Errorf("Error occurred when fetching map locations: %v", err)
+	}
+
+	// Set next config
+	c.Next = locations.Next
+	c.Previous = locations.Previous
+
+	// Print content
+	for _, loc := range locations.Results {
+		fmt.Println(loc.Name)
+	}
+
+	if onFirstPage == true {
+		fmt.Println("you're on the first page")
+	}
+
+	return nil
+}
